3c8c45ea73b58e1a76ddc7e1be838e5d0091bb63
khachmakhov <gotozero@ya.ru>
2019-11-01 22:21:56 +0300

    логинка, которая нихуя не работает спизжено отсюда:
    https://jasonwatmore.com/post/2019/06/10/angular-8-user-registration-and-login-example-tutorial

--- gagaton-site/src/app/app-routing.module.ts
+++ gagaton-site/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AppComponent} from "./app.component";
+import {AuthGuard} from "./_helpers";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  // { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gagaton-site/src/app/app.module.ts
+++ gagaton-site/src/app/app.module.ts
@@ -4,17 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import { LoginComponent } from './login/login.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ErrorInterceptor, fakeBackendProvider, JwtInterceptor} from "./_helpers";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
